aa35f9a2002aa6d1975546d028f80e059e6411f1
Pruthvi <pruthvi@users.noreply.github.com>
2018-10-19 11:18:21 -0400

    3D sound testing

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,31 @@
+﻿/*  Copyright (c) Pruthvi  |  http://pruthv.com  */
+
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+
+    #region Variables
+    private GameObject player;
+    public float speed;
+    Vector3 direction;
+    #endregion
+
+    void Start ()
+	{
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+        direction = player.transform.position - this.transform.position;
+
+    }
+
+    void Update ()
+	{
+
+        //set direction
+        // this.GetComponent<Rigidbody>().velocity = transform.forward + direction.normalized * 5;
+        // transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+        transform.LookAt(player.transform);
+        this.GetComponent<Rigidbody>().AddForce(direction * speed * Time.deltaTime);
+
+    }
+
+}
